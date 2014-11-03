0e5a1c3fa8296cffe2db8b4a1c7dbc004b045de8
John Foushee <hasuob@gmail.com>
2014-11-03 16:22:50 -0500

    Fixing typo

--- src/apply-schema.sh
+++ src/apply-schema.sh
@@ -2,11 +2,11 @@
 
 # The -d flag will import schema-data.sql instead of schema.sql
 
-vagrant ssh -c"mysql -uroot -p123 -e 'DROP DATABASE IF EXISTS lamarche; CREATE DATABASE lamarche;'"
+vagrant ssh -c"mysql -uroot -p123 -e 'DROP DATABASE IF EXISTS lamarche; CREATE DATABASE fzyskeleton;'"
 if [ "$1" = "-d" ]; then
     echo "Importing data..."
-    vagrant ssh -c"mysql -uroot -p123 lamarche < /vagrant/sql/schema-data.sql"
+    vagrant ssh -c"mysql -uroot -p123 fzyskeleton < /vagrant/sql/schema-data.sql"
 else
     echo "Importing schema..."
-    vagrant ssh -c"mysql -uroot -p123 lamarche < /vagrant/sql/schema.sql"
+    vagrant ssh -c"mysql -uroot -p123 fzyskeleton < /vagrant/sql/schema.sql"
 fi
