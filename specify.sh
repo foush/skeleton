923bb4cb44d6d3da2f43681f8079e97e3004d9e7
John Foushee <johnfoushee@gmail.com>
2014-11-03 15:33:30 -0500

    Bash bugfixes.

--- specify.sh
+++ specify.sh
@@ -2,17 +2,18 @@
 echo "Specifying project name"
 read -p "What is the lower case namespace of your project (eg fzyskeleton): " LCVAL
 find ./ -type f -exec sed -i -e "s/fzyskeleton/$LCVAL/g" {} \;
-read -p "What is the title case namespace of your project (eg FzySkeleton) : " TCVAL
-find ./ -type f -exec sed -i -e "s/fzyskeleton/$TCVAL/gi" {} \;
+read -p "What is the title case namespace of your project (eg FzySkeleton): " TCVAL
+find ./ -type f -exec sed -i -e "s/FzySkeleton/$TCVAL/g" {} \;
 
 read -n 1 -p "Set remote database connection variables for development?" setDevDB
 if [[ "$setDevDB" == "y" || "$setDevDB" == "Y" ]]; then
     while true; do
-         read -p "Development DB name" DEVDB
-         read -p "Development DB user" DEVUSER
-         read -p "Development DB pass" DEVPASS
-         read -p "Development DB host" DEVHOST
-         read -n 1 -p "Confirm\nDB: $DEVDB\nUser: $DEVUSER\nPass: $DEVPASS\nHOST: $DEVHOST" isCorrect
+         read -p "Development DB name: " DEVDB
+         read -p "Development DB user: " DEVUSER
+         read -p "Development DB pass: " DEVPASS
+         read -p "Development DB host: " DEVHOST
+         echo -e "DB: $DEVDB\nUser: $DEVUSER\nPass: $DEVPASS\nHOST: $DEVHOST\n"
+         read -n 1 -p "Confirm [y/n]: " isCorrect
          if [[ "$isCorrect" == "y" || "$isCorrect" == "Y" ]]; then
             echo "Configuring development connection..."
             sed -i -e "s/DEVDBNAME/$DEVDB/g" ./src/sync-qa-db.sh
@@ -26,11 +27,12 @@ fi
 read -n 1 -p "Set remote database connection variables for staging?" setQaDB
 if [[ "$setQaDB" == "y" || "$setQaDB" == "Y" ]]; then
     while true; do
-         read -p "Staging DB name" QADB
-         read -p "Staging DB user" QAUSER
-         read -p "Staging DB pass" QAPASS
-         read -p "Staging DB host" QAHOST
-         read -n 1 -p "Confirm\nDB: $QADB\nUser: $QAUSER\nPass: $QAPASS\nHOST: $QAHOST" isCorrect
+         read -p "Staging DB name: " QADB
+         read -p "Staging DB user: " QAUSER
+         read -p "Staging DB pass: " QAPASS
+         read -p "Staging DB host: " QAHOST
+         echo -e "DB: $DEVDB\nUser: $DEVUSER\nPass: $DEVPASS\nHOST: $DEVHOST\n"
+         read -n 1 -p "Confirm [y/n]: " isCorrect
          if [[ "$isCorrect" == "y" || "$isCorrect" == "Y" ]]; then
             echo "Configuring staging connection..."
             sed -i -e "s/QADBNAME/$QADB/g" ./src/sync-qa-db.sh
